b20c3b8605dcc56dbeaa0b815c67dab85ac35037
Edivaldo Reis <efrj@hotmail.com>
2023-04-30 17:33:23 -0300

    Added CowsayCharacters module

---
+++ lib/cowsay_characters.rb
@@ -0,0 +1,8 @@
+# lib/cowsay_characters.rb
+module CowsayCharacters
+  def self.available_characters
+    cow_files = `/usr/games/cowsay -l`.split("\n")[1..-1].join(" ").split(" ")
+    cow_files.map { |f| f.gsub('.cow', '') }
+  end
+end
+  
